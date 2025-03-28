d258d6ab515c58d1fdd5bad497c415d5fb84d5a3
ObayaDevOps <obayajamesdralega@gmail.com>
2025-03-28 12:10:43 +0100

    font test

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,19 +8,7 @@ import {  Unbounded, Lexend_Mega, Public_Sans, Archivo, DM_Sans, Red_Hat_Text,
   } from 'next/font/google'
 
 const neobrutalismFont1 = Unbounded({ subsets: [ 'latin' ], weight: ['400'] })
-const neobrutalismFont2 = Lexend_Mega({ subsets: [ 'latin' ], weight: ['600'] })
-const neobrutalismFont3 = Public_Sans({ subsets: [ 'latin' ], weight: ['400'] })
-const neobrutalismFont4 = Archivo({ subsets: [ 'latin' ], weight: ['400'] })
-const neobrutalismFont5Head = DM_Sans({ subsets: [ 'latin' ], weight: ['700'] })
-const neobrutalismFont5Text = DM_Sans({ subsets: [ 'latin' ], weight: ['400'] })
-const neobrutalismFont6 = Red_Hat_Text({ subsets: [ 'latin' ], weight: ['400'] })
-const neobrutalismFont7 = Work_Sans({ subsets: [ 'latin' ], weight: ['600'] }) //Other Good Option
 
-const logoFont = RocknRoll_One({ subsets: [ 'latin' ], weight: ['400'] })
-const logoFont2 = Zen_Kaku_Gothic_New({ subsets: [ 'latin' ], weight: ['400'] })
-const logoFont3= Dela_Gothic_One({ subsets: [ 'latin' ], weight: ['400'] })
-const logoFont4 = Noto_Serif({ subsets: [ 'latin' ], weight: ['400'] })
-const logoFont5 = Murecho({ subsets: [ 'latin' ], weight: ['400'] })
 
 
 
@@ -30,6 +18,9 @@ const system = createSystem(defaultConfig, {
           fonts: {
             heading: { value: `'Figtree', sans-serif` },
             body: { value: `'Figtree', sans-serif` },
+            neobrutalismFont1: neobrutalismFont1.style.fontFamily,
+
+
           },
         },
       },
