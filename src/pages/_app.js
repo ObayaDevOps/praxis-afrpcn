7090a67a20ae410c89f0543374210fe1d3a232c2
ObayaDevOps <obayajamesdralega@gmail.com>
2025-04-20 12:45:36 +0200

    added focus-vivble to remove boxes in mobile menu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,23 @@
 import { Provider } from "@/components/ui/provider"
 import theme from '../styles/theme'
 
+import { Global, css } from '@emotion/react'
+
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+     outline: none;
+     box-shadow: none;
+   }
+`;
+
 function App({ Component, pageProps }) {
   return (
     <Provider theme={theme}>
+        <Global styles={GlobalStyles} />
         <Component {...pageProps} />
     </Provider>
   )
